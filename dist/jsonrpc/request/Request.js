522bf2f096eaf5891b187a9fc52d3b436df4f436
Zhenya Kuzorov <zhenya.kuzorov@finsight.com>
2020-11-09 12:18:53 -0500

    Fix req->not dependencies

--- dist/jsonrpc/request/Request.js
+++ dist/jsonrpc/request/Request.js
@@ -7,46 +7,37 @@ exports.default = void 0;
 
 var _uuid = require("uuid");
 
-var _Notification2 = _interopRequireDefault(require("./Notification"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-var Request = /*#__PURE__*/function (_Notification) {
-  _inherits(Request, _Notification);
-
-  var _super = _createSuper(Request);
-
+var Request = /*#__PURE__*/function () {
   /**
    * @param {object} payload
    */
   function Request(payload) {
-    var _this;
-
     _classCallCheck(this, Request);
 
-    _this = _super.call(this, payload);
+    this.jsonrpc = '2.0';
+
+    if (!payload.method) {
+      throw Error('No method specified for request');
+    }
+    /**
+     * @type {string}
+     */
+
+
+    this.method = payload.method;
+
+    if (payload.params) {
+      /**
+       * @type {object|array}
+       */
+      this.params = payload.params;
+    }
 
     if (!payload.id && payload.id !== null) {
       payload.id = (0, _uuid.v4)();
@@ -56,8 +47,7 @@ var Request = /*#__PURE__*/function (_Notification) {
      */
 
 
-    _this.id = payload.id;
-    return _this;
+    this.id = payload.id;
   }
   /**
    * Get request id
@@ -71,9 +61,51 @@ var Request = /*#__PURE__*/function (_Notification) {
     value: function getId() {
       return this.id;
     }
+    /**
+     * Get rpc method
+     *
+     * @return {string}
+     */
+
+  }, {
+    key: "getMethod",
+    value: function getMethod() {
+      return this.method;
+    }
+    /**
+     * @return {object|array}
+     */
+
+  }, {
+    key: "getParams",
+    value: function getParams() {
+      return this.params;
+    }
+    /**
+     * Get contents of result.headers
+     *
+     * @return {string|Headers}
+     */
+
+  }, {
+    key: "getHeaders",
+    value: function getHeaders() {
+      return this.params.headers;
+    }
+    /**
+     * Get contents of result.payload
+     *
+     * @return {*}
+     */
+
+  }, {
+    key: "getPayload",
+    value: function getPayload() {
+      return this.params.payload;
+    }
   }]);
 
   return Request;
-}(_Notification2.default);
+}();
 
 exports.default = Request;
--- src/jsonrpc/request/Request.js
+++ src/jsonrpc/request/Request.js
@@ -1,12 +1,26 @@
 import { v4 as uuid } from 'uuid';
-import Notification from './Notification';
 
-export default class Request extends Notification {
+export default class Request {
   /**
    * @param {object} payload
    */
   constructor(payload) {
-    super(payload);
+    this.jsonrpc = '2.0';
+    if (!payload.method) {
+      throw Error('No method specified for request');
+    }
+
+    /**
+     * @type {string}
+     */
+    this.method = payload.method;
+
+    if (payload.params) {
+      /**
+       * @type {object|array}
+       */
+      this.params = payload.params;
+    }
 
     if (!payload.id && payload.id !== null) {
       payload.id = uuid();
@@ -25,5 +39,39 @@ export default class Request extends Notification {
    */
   getId() {
     return this.id;
+  }
+
+  /**
+   * Get rpc method
+   *
+   * @return {string}
+   */
+  getMethod() {
+    return this.method;
+  }
+
+  /**
+   * @return {object|array}
+   */
+  getParams() {
+    return this.params;
+  }
+
+  /**
+   * Get contents of result.headers
+   *
+   * @return {string|Headers}
+   */
+  getHeaders() {
+    return this.params.headers;
+  }
+
+  /**
+   * Get contents of result.payload
+   *
+   * @return {*}
+   */
+  getPayload() {
+    return this.params.payload;
   }
 }
